994b7b3223f27884265a4d2fd3224dffe43e3590
smorin8674 <sarahmorin@gwu.edu>
2020-02-26 15:59:10 -0500

    Updates per review of PR.
    Module level docstring added to each module. Currently they are very
    short, one line descriptions. Wording fixed in several places.
    max_to_draw changed to float max_fraction_to_draw. Tests have been
    updated to reflect this. The abstract compute_risk and
    compute_sample_size methods have been updated to include variable
    arguments so that derived class implementations will not present
    problems when calling those methods. Simple __init__ has been added to
    Election which accepts a list of Contests.

--- src/r2b2/audit.py
+++ src/r2b2/audit.py
@@ -1,3 +1,4 @@
+"""Abstract module defining an Audit framework."""
 from abc import ABC
 from abc import abstractmethod
 
@@ -7,16 +8,16 @@ from r2b2.contest import Contest
 class Audit(ABC):
     """Abstract Base Class to define a general Audit object type.
 
-    The Audit class will be an abstract base class which defines the general structure and properties
-    of a risk-limiting audit. Individual RLAs should be subclasses of the Audit class.
+    The Audit class is an abstract base class which defines the general structure and properties
+    of a risk-limiting audit. Individual RLAs are subclasses of the Audit class.
 
     Attributes:
         alpha (float): Risk limit.  Alpha represents the chance that given an incorrectly called
             election, the audit will fail to go to a full recount.
         beta (float): the worst case chance of causing an unnecessary full recount. For many RLAs,
             beta will simply be set to 0 and will not appear to be a parameter.
-        max_to_draw (integer): The maximum total number of ballots auditors are willing to draw
-            during the course of the audit.
+        max_fraction_to_draw (float): The maximum total number of ballots auditors are willing to
+            draw during the course of the audit.
         replacement (bool): Indicates if the audit sampling should be done with (true) or without
             (false) replacement.
         contest (Contest): Contest on which to run the audit.
@@ -24,11 +25,11 @@ class Audit(ABC):
 
     alpha: float
     beta: float
-    max_to_draw: int
+    max_fraction_to_draw: float
     replacement: bool
     contest: Contest
 
-    def __init__(self, alpha: float, beta: float, max_to_draw: int,
+    def __init__(self, alpha: float, beta: float, max_fraction_to_draw: float,
                  replacement: bool, contest: Contest):
         """Create an instance of an Audit.
 
@@ -45,26 +46,30 @@ class Audit(ABC):
             raise TypeError('beta must be a float.')
         if beta < 0 or beta > 1.0:
             raise ValueError('beta must be between 0 and 1.')
-        if type(max_to_draw) is not int:
-            raise TypeError('max_to_draw must be an integer.')
-        if max_to_draw < 0:
-            raise ValueError('max_to_draw must be non-negative.')
+        if type(max_fraction_to_draw) is not float:
+            raise TypeError(
+                'max_fraction_to_draw must be a fraction (i.e. float).')
+        if max_fraction_to_draw < 0 or max_fraction_to_draw > 1:
+            raise ValueError('max_fraction_to_draw must be between 0 and 1')
         if type(replacement) is not bool:
             raise TypeError('replacement must be boolean.')
         if contest is not None:
             if type(contest) is not Contest:
                 raise TypeError('contest must be a Contest object')
-            if max_to_draw > contest.total_ballots_cast:
-                raise ValueError('max_to_draw cannot be more than the total ballots cast in the contest.')
+            if max_fraction_to_draw > contest.total_ballots_cast:
+                raise ValueError(
+                    'max_fraction_to_draw cannot be more than the total ballots cast in the contest.'
+                )
+        # TODO: Handle cases where Contest is None.
 
         self.alpha = alpha
         self.beta = beta
-        self.max_to_draw = max_to_draw
+        self.max_fraction_to_draw = max_fraction_to_draw
         self.replacement = replacement
         self.contest = contest
 
     @abstractmethod
-    def compute_risk(self):
+    def compute_risk(self, *args, **kwargs):
         """Compute the current risk level of the audit.
 
         Returns:
@@ -74,7 +79,7 @@ class Audit(ABC):
         pass
 
     @abstractmethod
-    def compute_sample_size(self):
+    def compute_sample_size(self, *args, **kwargs):
         """Compute the next sample size to draw."""
 
         pass
--- src/r2b2/tests/test_audit.py
+++ src/r2b2/tests/test_audit.py
@@ -7,8 +7,8 @@ from r2b2.contest import Contest
 class SimpleAudit(Audit):
     """For testing purposes only."""
 
-    def __init__(self, alpha: float, beta: float, max_to_draw: int, replacement: bool, contest: Contest):
-        super().__init__(alpha, beta, max_to_draw, replacement, contest)
+    def __init__(self, alpha: float, beta: float, max_fraction_to_draw: int, replacement: bool, contest: Contest):
+        super().__init__(alpha, beta, max_fraction_to_draw, replacement, contest)
 
     def compute_risk(self):
         return 0.1
@@ -18,10 +18,10 @@ class SimpleAudit(Audit):
 
 
 def test_simple_audit():
-    simpleaudit1 = SimpleAudit(0.1, 0.05, 200, True, None)
+    simpleaudit1 = SimpleAudit(0.1, 0.05, 0.1, True, None)
     assert simpleaudit1.alpha == 0.1
     assert simpleaudit1.beta == 0.05
-    assert simpleaudit1.max_to_draw == 200
+    assert simpleaudit1.max_fraction_to_draw == 0.1
     assert simpleaudit1.replacement
     assert simpleaudit1.contest is None
     assert simpleaudit1.compute_risk() == 0.1
@@ -29,33 +29,44 @@ def test_simple_audit():
 
 
 def test_initialization_errors():
+    # alpha TypeError tests
     with pytest.raises(TypeError):
-        SimpleAudit('a', 0.05, 200, True, None)
+        SimpleAudit('a', 0.05, 0.1, True, None)
     with pytest.raises(TypeError):
-        SimpleAudit(True, 0.05, 200, True, None)
+        SimpleAudit(True, 0.05, 0.1, True, None)
     with pytest.raises(TypeError):
-        SimpleAudit(22, 0.05, 200, True, None)
+        SimpleAudit(22, 0.05, 0.1, True, None)
+    # beta TypeError tests
     with pytest.raises(TypeError):
-        SimpleAudit(0.1, 'b', 200, True, None)
+        SimpleAudit(0.1, 'b', 0.1, True, None)
     with pytest.raises(TypeError):
-        SimpleAudit(0.1, False, 200, True, None)
+        SimpleAudit(0.1, False, 0.1, True, None)
     with pytest.raises(TypeError):
-        SimpleAudit(0.1, -3, 200, True, None)
+        SimpleAudit(0.1, -3, 0.1, True, None)
+    # max_fraction_to_draw TypeError tests
     with pytest.raises(TypeError):
         SimpleAudit(0.1, 0.05, 'c', True, None)
     with pytest.raises(TypeError):
-        SimpleAudit(0.1, 0.05, 2.5, True, None)
+        SimpleAudit(0.1, 0.05, 2, True, None)
     with pytest.raises(TypeError):
         SimpleAudit(0.1, 0.5, False, True, None)
+    # replacement TypeError tests
     with pytest.raises(TypeError):
-        SimpleAudit(0.1, 0.05, 200, 10, None)
+        SimpleAudit(0.1, 0.05, 0.1, 10, None)
     with pytest.raises(TypeError):
-        SimpleAudit(0.1, 0.05, 200, 'd', None)
+        SimpleAudit(0.1, 0.05, 0.1, 'd', None)
+    # alpha ValueError tests
     with pytest.raises(ValueError):
-        SimpleAudit(-1.5, 0.05, 200, True, None)
+        SimpleAudit(-1.5, 0.05, 0.1, True, None)
     with pytest.raises(ValueError):
-        SimpleAudit(2.4, 0.05, 200, True, None)
+        SimpleAudit(2.4, 0.05, 0.1, True, None)
+    # beta ValueError tests
     with pytest.raises(ValueError):
-        SimpleAudit(0.1, -2.5, 200, True, None)
+        SimpleAudit(0.1, -2.5, 0.1, True, None)
     with pytest.raises(ValueError):
-        SimpleAudit(0.1, 5.3, 200, True, None)
+        SimpleAudit(0.1, 5.3, 0.1, True, None)
+    # max_fraction_to_draw ValueError tests
+    with pytest.raises(ValueError):
+        SimpleAudit(0.1, 0.05, -0.1, True, None)
+    with pytest.raises(ValueError):
+        SimpleAudit(0.1, 0.05, 1.5, True, None)
