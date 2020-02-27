c114d999e897150342915d41a3fc88d663ba1c6b
smorin8674 <sarahmorin@gwu.edu>
2020-02-27 12:18:18 -0500

    Added parameter checking.
    Added basic parameter checking to Contest object creation. Tests being
    developed.

--- src/r2b2/contest.py
+++ src/r2b2/contest.py
@@ -39,6 +39,39 @@ class Contest:
     def __init__(self, contest_ballots: int, tally: Dict[str, int],
                  num_winners: int, reported_winners: List[str],
                  contest_type: ContestType):
+
+        if type(contest_ballots) is not int:
+            raise TypeError('contest_ballots must be integer.')
+        if contest_ballots < 1:
+            raise ValueError('contest_ballots must be at least 1.')
+        if type(tally) is not dict:
+            raise TypeError('tally must be a dict.')
+        else:
+            for k, v in tally.items():
+                if type(k) is not str or type(v) is not int:
+                    raise TypeError(
+                        'tally must contain str keys and int values')
+        if sum(tally.values()) > contest_ballots:
+            raise ValueError('tally contains more than contest_ballots.')
+        if sum(tally.values()) < 1:
+            raise ValueError(
+                'tally must contain a total of at least 1 ballot.')
+        if type(num_winners) is not int:
+            raise TypeError('num_winners must be integer.')
+        if num_winners < 1 or num_winners > len(tally):
+            raise ValueError(
+                'num_winners must be between 1 and number of candidates.')
+        if type(reported_winners) is not list:
+            raise TypeError('reported_winners must be a list[str].')
+        else:
+            for w in reported_winners:
+                if type(w) is not str:
+                    raise TypeError('reported_winners must be a list[str].')
+                if w not in tally.keys():
+                    raise ValueError('reported winners must be candidates.')
+        if type(contest_type) is not ContestType:
+            raise TypeError('contest_type must be ContestType Enum object.')
+
         self.contest_ballots = contest_ballots
         self.tally = tally
         self.num_winners = num_winners
