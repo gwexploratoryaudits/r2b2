25e8b7b7b81bab09a3ab50d9158b221d874ac74f
smorin8674 <sarahmorin@gwu.edu>
2020-02-29 13:35:56 -0500

    Added methods to parse contest, election from json
    Added methods for parsing a list of contests or an election from JSON
    file. These should make it easier to design and implement tests.
    Currently the contest method will produce a list of contests using the
    format given in contest_template.json and the election method will parse
    a single election (containing a list of contests) using the format given
    in election_template.json. There is no real edge case testing for either
    of these methods, not sure how much of this needs to be added since they
    should only be used for tests developers write.

--- src/r2b2/tests/util.py
+++ src/r2b2/tests/util.py
@@ -1,3 +1,4 @@
+import json
 from random import randint
 
 from r2b2.contest import Contest
@@ -19,6 +20,7 @@ def generate_contest(max_size):
 
 
 def generate_election(max_size, max_contests=None):
+    """Generate an Election with random data. For testing purposes only."""
     name = 'TestElection' + str(randint(1, 100))
     total_ballots = randint(1, max_size)
     if max_contests is None:
@@ -27,5 +29,55 @@ def generate_election(max_size, max_contests=None):
     contests = list()
     for i in range(num_contests):
         contests.append(generate_contest(total_ballots))
+
+    return Election(name, total_ballots, contests)
+
+
+def parse_contest_list(json_file):
+    """Parse a list of Contests from a JSON file.
+
+    Note:
+        Template for Contest format in JSON in contest_template.json
+    """
+    with open(json_file, 'r') as json_data:
+        data = json.load(json_data)
+
+    contests = []
+
+    for contest in data:
+        contest_ballots = data[contest]['contest_ballots']
+        tally = data[contest]['tally']
+        num_winners = data[contest]['num_winners']
+        reported_winners = data[contest]['reported_winners']
+        contest_type = ContestType[data[contest]['contest_type']]
+        contests.append(
+            Contest(contest_ballots, tally, num_winners, reported_winners,
+                    contest_type))
+
+    return contests
+
+
+def parse_election(json_file):
+    """Parse an Eleciton from a JSON file.
+
+    Note:
+        Templace for Election JSON format in election_template.json
+    """
+    with open(json_file, 'r') as json_data:
+        data = json.load(json_data)
+
+    name = data['name']
+    total_ballots = data['total_ballots']
+    contests = []
+
+    for contest in data['contests']:
+        contest_ballots = data['contests'][contest]['contest_ballots']
+        tally = data['contests'][contest]['tally']
+        num_winners = data['contests'][contest]['num_winners']
+        reported_winners = data['contests'][contest]['reported_winners']
+        contest_type = ContestType(data['contests'][contest]['contest_type'])
+        contests.append(
+            Contest(contest_ballots, tally, num_winners, reported_winners,
+                    contest_type))
 
     return Election(name, total_ballots, contests)
