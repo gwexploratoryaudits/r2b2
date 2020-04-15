0ff720794ee1deb5d949e2e6c9be2b41a3912112
sarahmorin <sarahmorin@gwu.edu>
2020-04-15 12:19:43 -0400

    addded brla test. fixed error in binary search.
    added brla test for data from BRLA paper table 3. found an error in
    binary search in next_min_winner_ballots() for cases where left = right
    going unchecked.

--- src/r2b2/tests/test_brla.py
+++ src/r2b2/tests/test_brla.py
@@ -1,7 +1,10 @@
 import pytest
+import json
 
 import r2b2.tests.util as util
 from r2b2.brla import BayesianRLA as BRLA
+from r2b2.contest import Contest
+from r2b2.contest import ContestType
 
 default_contest = util.generate_contest(100)
 
@@ -39,3 +42,17 @@ def test_exceptions():
         simplebrla.compute_all_min_winner_ballots(0)
     with pytest.raises(ValueError):
         simplebrla.compute_all_min_winner_ballots(1000)
+
+
+def test_paper_data():
+    """Test brla using data from BRLA paper Table 3."""
+    with open('src/r2b2/tests/brla_test_data.json', 'r') as json_file:
+        data = json.load(json_file)
+
+    rounds = data['rounds']
+    contest = Contest(100000, {'A':60000, 'B':40000}, 1, ['A'], ContestType.PLURALITY)
+    
+    for test in data['test_cases']:
+        audit = BRLA(data['test_cases'][test]['risk'], 1.0, contest)
+        kmins = audit.compute_min_winner_ballots(rounds)
+        assert kmins == data['test_cases'][test]['kmins']
