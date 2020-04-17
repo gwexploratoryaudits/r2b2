7056846b06620788ce5cb8cb0a16255e0c5f3201
Oliver Broadrick <odbroadrick@gmail.com>
2020-04-17 13:09:52 -0400

    Add compute_all and get_min_sample.
    get_mine_sample_size is added (same as in brla.py) as well as get_min_sample_size(). Also now return -1 in compute_next for invalid sample_sizes (kmin > sample_size).

--- src/r2b2/brla_replace.py
+++ src/r2b2/brla_replace.py
@@ -36,6 +36,27 @@ class BayesianRLA(Audit):
 
         super().__init__(alpha, 0.0, max_fraction_to_draw, False, contest)
         self.prior = self.compute_prior()
+        self.min_sample_size = self.get_min_sample_size()
+
+    def get_min_sample_size(self):
+        left = 1
+        right = math.ceil(self.contest.contest_ballots *
+                          self.max_fraction_to_draw)
+
+        while left < right:
+            proposed_min = (left + right) // 2
+            proposed_min_kmin = self.next_min_winner_ballots(proposed_min)
+
+            if proposed_min_kmin == -1:
+                left = proposed_min + 1
+            else:
+                previous_kmin = self.next_min_winner_ballots(proposed_min - 1)
+                if previous_kmin == -1:
+                    return proposed_min
+                else:
+                    right = proposed_min - 1
+        # FIXME: need check for final size, if we reach this is the contest to small for the audit?
+        return left
 
     def stopping_condition(self, votes_for_winner: int) -> bool:
         if len(self.rounds) < 1:
@@ -51,16 +72,13 @@ class BayesianRLA(Audit):
             sample_size (int): Current round size, i.e. number of ballots to be sampled in round
 
         Returns:
-            An int which represents the minimum number of votes cast for the reported winner in the
-            current round size in order to stop the audit during that round.
+            int: The minimum number of votes cast for the reported winner in the
+            current round size in order to stop the audit during that round. -1 if
+            the sample_size is invalid.
         """
 
-        if sample_size < 1:
-            raise ValueError('Sample size must be at least 1.')
-        if sample_size > self.contest.contest_ballots * self.max_fraction_to_draw:
-            raise ValueError(
-                'Sample size cannot be larger than max fraction to draw of the contest ballots.'
-            )
+        if sample_size < self.min_sample_size:
+            return -1
 
         left = math.floor(sample_size / 2)
         right = sample_size
@@ -83,9 +101,12 @@ class BayesianRLA(Audit):
             else:
                 left = proposed_stop + 1
 
-        # FIXME: Should we test final size when left = right and return some default if
-        # round size is too small?
-        return left
+        # Handle case where kmin = sample_size
+        proposed_stop_risk = self.compute_risk(sample_size, sample_size)
+        if proposed_stop_risk <= self.alpha:
+            return sample_size
+        # Otherwise kmin > sample_size, so we return -1
+        return -1
 
     def compute_prior(self) -> np.ndarray:
         """Compute prior distribution of worst case election."""
@@ -171,5 +192,45 @@ class BayesianRLA(Audit):
         for sample_size in self.rounds:
             min_winner_ballots.append(
                 self.next_min_winner_ballots(sample_size))
+
+        return min_winner_ballots
+
+    def compute_all_min_winner_ballots(self,
+                                       max_sample_size: int = None,
+                                       *args,
+                                       **kwargs):
+        """Compute the minimum winner ballots for all possible sample sizes.
+
+        Args:
+            max_sample_size (int): Optional. Set maximum sample size to generate stopping sizes up
+                to. If not provided the maximum sample size is determined by max_fraction_to_draw
+                and the total contest ballots.
+
+        Returns:
+            List[int]: List of minimum winner ballots to meet the stopping condition for each round
+                size in the range [min_sample_size, max_sample_size].
+        """
+
+        if max_sample_size is None:
+            max_sample_size = math.ceil(self.contest.contest_ballots *
+                                        self.max_fraction_to_draw)
+        if max_sample_size < self.min_sample_size:
+            raise ValueError(
+                'Maximum sample size must be greater than or equal to minimum size.'
+            )
+        if max_sample_size > self.contest.contest_ballots:
+            raise ValueError(
+                'Maximum sample size cannot exceed total contest ballots.')
+
+        current_kmin = self.next_min_winner_ballots(self.min_sample_size)
+        min_winner_ballots = [current_kmin]
+
+        # For each additional ballot, the kmin can only increase by
+        for sample_size in range(self.min_sample_size + 1,
+                                 max_sample_size + 1):
+            if self.compute_risk(current_kmin, sample_size) > self.alpha:
+                current_kmin += 1
+
+            min_winner_ballots.append(current_kmin)
 
         return min_winner_ballots
