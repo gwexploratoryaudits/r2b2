4bac5e8536f30ed09b222b247cb4cb92fa291661
sarahmorin <sarahmorin@gwu.edu>
2020-04-22 14:18:15 -0400

    initial cli bulk implementation.
    added flags and functionality for generating lists of kmins using one of
    the bulk audit methods. no test files added.

--- src/r2b2/cli.py
+++ src/r2b2/cli.py
@@ -15,6 +15,7 @@ Why does this file exist, and why not put this in __main__?
   Also see (1) from http://click.pocoo.org/5/setuptools/#setuptools-integration
 """
 import argparse
+import math
 
 from r2b2.audit import Audit
 from r2b2.brla import BayesianRLA as BRLA
@@ -67,6 +68,27 @@ parser.add_argument('-m',
                     dest='max_fraction_to_draw',
                     default=None,
                     type=float)
+parser.add_argument('-l',
+                    '--round-list',
+                    metavar='ROUND LIST',
+                    help='Provide a list of round sizes for bulk computation.',
+                    dest='rounds',
+                    default=None,
+                    nargs='+',
+                    type=int)
+parser.add_argument('-f',
+                    '--full-audit-limit',
+                    metavar='FULL AUDIT MAXIMUM SIZE',
+                    help='Set maximum size of full audit in bulk mode.',
+                    dest='max_size',
+                    type=int,
+                    default=None)
+parser.add_argument('-o',
+                    '--output',
+                    metavar='OUTPUT FILE',
+                    help='Write output to a given file in bulk mode.',
+                    dest='output',
+                    default=None)
 
 
 def main(args=None):
@@ -87,9 +109,52 @@ def main(args=None):
             audit = input_audit(contest, args.alpha, args.max_fraction_to_draw,
                                 args.audit_type)
             audit.run()
+    elif args.election_mode:
+        print('Bulk Election mode is currently unavailable.')
+        # TODO: implement election auditing
+        return
     else:
-        # TODO: implement non-interactive option
-        print('Bulk Auditing tool not currently available.')
+        # Get contest from contest file
+        if args.contest_file is None:
+            raise Exception('Contest file must be provided in bulk mode.')
+        contest = util.parse_contest(str(args.contest_file))
+        # Ensure all Audit parameters are given
+        if args.audit_type is None:
+            raise Exception('Audit type must be provided in bulk mode.')
+        if args.max_fraction_to_draw is None:
+            raise Exception('Max fraction to draw must be provided in bulk mode.')
+        if args.alpha is None:
+            raise Exception('Risk-limit must be provided in bulk mode.')
+        # Create audit based on type
+        # If any additional args are needed, parse and verify here
+        if args.audit_type == 'brla':
+            audit = BRLA(args.alpha, args.max_fraction_to_draw, contest)
+        else:
+            raise Exception('Audit type must be a valid choice.')
+        # Determine if mode is rounds, list, or full list
+        # Create formatted output
+        out = '\n{:^20}|{:^20}\n'.format('Round Sizes', 'Stopping Sizes')
+        out += '--------------------|--------------------\n'
+        if args.rounds is not None:
+            kmins = audit.compute_min_winner_ballots(args.rounds)
+            for i in range(len(kmins)):
+                out += '{:^20}|{:^20}\n'.format(args.rounds[i], kmins[i])
+        elif args.max_size is not None:
+            kmins = audit.compute_all_min_winner_ballots(args.max_size)
+            for r in range(audit.min_sample_size, args.max_size + 1):
+                out += '{:^20}|{:^20}\n'.format(r, kmins[r-audit.min_sample_size])
+        else:
+            max_size = math.ceil(audit.max_fraction_to_draw * audit.contest.contest_ballots)
+            kmins = audit.compute_all_min_winner_ballots()
+            for r in range(audit.min_sample_size, max_size + 1):
+                out += '{:^20}|{:^20}\n'.format(r, kmins[r-audit.min_sample_size])
+        # Either print output or write to given file
+        if args.output is not None:
+            with open(args.output, 'w') as out_file:
+                out_file.write(out)
+                out_file.close()
+        else:
+            print(out)
 
 
 def input_audit(contest: Contest,
