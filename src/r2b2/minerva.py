fda7a904a89b9026612c65fb031a5599cdb99de4
GrantMcClearn <32199865+GrantMcClearn@users.noreply.github.com>
2020-04-27 12:56:48 -0400

    Implement non-interactive Minerva
    - Implement Minerva for predetermined round schedules
    - Change current_dist_* functions

--- src/r2b2/minerva.py
+++ src/r2b2/minerva.py
@@ -1,5 +1,6 @@
 """Minerva audit module."""
 import math
+from typing import List
 
 from r2b2.audit import Audit
 from r2b2.contest import Contest
@@ -51,16 +52,64 @@ class Minerva(Audit):
         pass
 
     def stopping_condition(self, votes_for_winner: int) -> bool:
+        # NOTE: Before computation of kmin.
         pass
 
     def next_min_winner_ballots(self, sample_size) -> int:
         pass
 
-    def compute_min_winner_ballots(self, *args, **kwargs):
-        pass
+    def compute_min_winner_ballots(self, rounds: List[int], *args, **kwargs):
+        """Compute the minimum number of winner ballots for a round schedule.
+
+        Extend the audit's round schedule with the passed (partial) round schedule, and then extend
+        the audit's minimum number of winner ballots schedule with the corresponding minimums to
+        meet the stopping condition.
+
+        Args: rounds (List[int]): A (partial) round schedule of the audit.
+        """
+
+        if len(rounds) < 1:
+            raise ValueError('Round schedule must contain at least 1 round.')
+
+        if len(self.rounds) > 0 and rounds[0] <= self.rounds[-1]:
+            raise ValueError('Sample sizes must exceed past sample sizes.')
+
+        for i in range(0, len(rounds)):
+            if rounds[i] < self.min_sample_size:
+                raise ValueError('Sample size must be >= minimum sample size.')
+            if rounds[i] > self.contest.contest_ballots * self.max_fraction_to_draw:
+                raise ValueError(
+                    'Sample size cannot exceed the maximum fraction of contest ballots to draw.')
+            if i >= 1 and rounds[i] <= rounds[i - 1]:
+                raise ValueError('Round schedule is cumulative and so must strictly increase.')
+
+        for sample_size in rounds:
+            self.rounds.append(sample_size)
+            self.current_dist_null()
+            self.current_dist_reported()
+            self.find_kmin()
+            self.truncate_dist_null()
+            self.truncate_dist_reported()
+
+    def find_kmin(self):
+        """Search for a kmin (minimum number of winner ballots) satisfying all stopping criteria."""
+
+        for possible_kmin in range(self.rounds[-1] // 2 + 1, len(self.distribution_null)):
+            tail_null = sum(self.distribution_null[possible_kmin:])
+            tail_reported = sum(self.distribution_reported_tally[possible_kmin:])
+
+            # Minerva's stopping criterion: tail_reported / tail_null > 1 / alpha.
+            if self.alpha * tail_reported >= tail_null:
+                self.min_winner_ballots.append(possible_kmin)
+                return
+
+        # Sentinel of None plays nice with truncation.
+        self.min_winner_ballots.append(None)
 
     def compute_all_min_winner_ballots(self, *args, **kwargs):
         pass
 
     def compute_risk(self, *args, **kwargs):
+        # NOTE: For interactive: minimum of 1/ratio (where numerator is realized ballots for winner)
+        # schedule. For non-interactive/bulk: possibly the same?. No ratio schedule field here yet.
         pass
