6d0b57557c565fbceaefb95985b85f57e071f992
sarahmorin <sarahmorin@gwu.edu>
2020-05-05 14:26:10 -0400

    improved documentation and help strings.
    docstrings now provide help text for click interface and docstrings
    formatted for RTD. the RTD docstrings are truncated from the cli help
    text, as they should be. added template command to generate contest and
    election json file templates for ease of use.

--- src/r2b2/cli.py
+++ src/r2b2/cli.py
@@ -1,22 +1,26 @@
 """
-Module that contains the command line app.
+R2B2's command line interface offers significant out-of-the-box functionality with
+respect to executing audits and generating audit data without requiring the user to
+write a single line of Python.
 
-Why does this file exist, and why not put this in __main__?
+Note:
+    Why does this file exist, and why not put this in __main__?
 
-  You might be tempted to import things from __main__ later, but that will cause
-  problems: the code will get executed twice:
+      You might be tempted to import things from __main__ later, but that will cause
+      problems: the code will get executed twice:
 
-  - When you run `python -m r2b2` python will execute
-    ``__main__.py`` as a script. That means there won't be any
-    ``r2b2.__main__`` in ``sys.modules``.
-  - When you import __main__ it will get executed again (as a module) because
-    there's no ``r2b2.__main__`` in ``sys.modules``.
+      - When you run `python -m r2b2` python will execute
+        ``__main__.py`` as a script. That means there won't be any
+        ``r2b2.__main__`` in ``sys.modules``.
+      - When you import __main__ it will get executed again (as a module) because
+        there's no ``r2b2.__main__`` in ``sys.modules``.
 
-  Also see (1) from http://click.pocoo.org/5/setuptools/#setuptools-integration
+      Also see (1) from http://click.pocoo.org/5/setuptools/#setuptools-integration
 """
 import math
 
 import click
+import pkg_resources
 
 from r2b2.audit import Audit
 from r2b2.brla import BayesianRLA as BRLA
@@ -34,7 +38,7 @@ class IntList(click.ParamType):
             str_list = value.split(' ')
             return [int(i) for i in str_list]
         except TypeError:
-            self.fail('Expected space separatedlist of integers')
+            self.fail('Expected space separated list of integers')
 
 
 INT_LIST = IntList()
@@ -83,13 +87,39 @@ def cli():
 def interactive(election_mode, election_file, contest_file, audit_type, risk_limit, max_fraction_to_draw, verbose):
     """Executes an audit round by round.
 
-    \b
-    In interactive mode users will be prompted for:
-        - Contest results (unless a contest file is provided)
-        - Audit type (if not provided with -a)
-        - Risk Limit (if not provided with -r)
-        - Maximum fraction of contest ballots to draw (if not given with -m)
-        - Each round size
+    Depending on what options are passed to the interactive command, users may be prompted for
+    contest results, audit type, risk limit, and/or maximum fraction of contest ballots to draw
+    when initializing the contest and audit to run.
+
+    During execution, users will enter each round
+    size and results of the round's sample and subsequently receive information about the current
+    state of the audit. The process continues until either the stopping conditions are met or the
+    audit reaches the maximum sample size.
+    \f
+
+    For information on each option run ::
+
+        $ r2b2 interactive --help
+
+    Example:
+        Contest results can be passed as a JSON file rather than entering the data through the
+        prompt::
+
+            $ r2b2 interactive --contest-file example_contest.json
+
+    Tip:
+        To generate a template contest JSON file run::
+
+            $ r2b2 template contest
+
+    Example:
+        Audit parameters can be passed in as options rather than entering through the prompt::
+
+            $ r2b2 interactive --audit-type brla --risk-limit 0.1 --max-fraction-to-draw 0.2
+            $ r2b2 interactive -a brla -r 0.1 -m 0.2    // Shortened equivalent
+
+    Warning:
+        Election mode is not yet implemented.
     """
 
     if election_mode:
@@ -143,14 +173,47 @@ def bulk(audit_type, risk_limit, max_fraction_to_draw, contest_file, output, rou
     Either provide a list of round sizes for which to generate stopping sizes or
     generate a ballot by ballot list of stopping sizes from the minimum valid sample
     size to the default maximum sample size or a specified maximum sample size.
+    \f
 
-    \b
-    For bulk execution, the user provides the following arguments:
-    - AUDIT_TYPE            Which type of audit to use to generate stopping sizes
-    - RISK_LIMIT            Risk limit (alpha) provided to audit.
-    - MAX_FRACTION_TO_DRAW  Maximum fraction of contest ballots that could be drawn during the audit.
-                            This sets the default maximum size for the ballot by ballot result.
-    - CONTEST_FILE          Contest data as a JSON file.
+    Args:
+        contest_file: Contest results as JSON file.
+        audit_type: Which audit type to use to generate stopping sizes.
+        risk_limit: Risk limit (alpha) of audit.
+        max_fraction_to_draw: Maximum fraction of contest ballots that could be drawm during
+            the audit. Sets the default maximum size of the ballot by ballot output.
+
+    Tip:
+        To generate a template contest JSON file, run::
+
+            $ r2b2 template contest
+
+    Returns:
+        Formatted list of rounds and their associated stopping sizes. Default execution is
+        ballot by ballot from minimum valid sample size to the maximum sample size of audit.
+
+    Example:
+        To generate stopping sizes for a specific set of round sizes, provide the round sizes
+        as a space separated list of integers enclosed by quotes using the round list option::
+
+            $ r2b2 bulk -l '100 200 300' contest.json brla 0.1 0.5
+
+    Example:
+        To generate a ballot by ballot result from the minimum valid sample size to a specific
+        maximum (i.e. not the maximum fraction to draw of the audit), run::
+
+            $ r2b2 bulk -f 221 contest.json brla 0.1 0.5
+
+    Example:
+        To write the results to a file instead of to stdout, run::
+
+            $ r2b2 bulk -o output.txt contest.json brla 0.1 0.5
+
+    Tip:
+        Generating large or compute heavy data sets can take some time. To estimate run times,
+        use the verbose flag to display a progress bar::
+
+            $ r2b2 bulk -v contest.json brla 0.1 0.5
+
     """
     # Parse audit and contest from arguments
     contest = util.parse_contest(contest_file)
@@ -181,8 +244,48 @@ def bulk(audit_type, risk_limit, max_fraction_to_draw, contest_file, output, rou
         click.echo(out)
 
 
+@cli.command('template', short_help='Generate template JSON input files')
+@click.argument('style', type=click.Choice(['contest', 'election']))
+@click.option('-o', '--output', type=click.File('wb'), default=None, help='Write output into given file.')
+def template(style, output):
+    """Generate JSON templates for possible input formats.
+    \f
+
+    Example:
+        To create a contest results JSON file, first generate the template as a new JSON file::
+
+            $ r2b2 template -o my_contest.json contest
+
+        Now the file my_contest.json will be created and contain::
+
+            {
+                "contest_ballots" : 100,
+                "tally" : {
+                        "CandidateA" : 50,
+                        "CandidateB" : 50
+                },
+                "num_winners" : 1,
+                "reported_winners" : ["CandidateA"],
+                "contest_type" : "PLURALITY"
+            }
+
+        Simply repopulate the fields with your contest results.
+    """
+    # TODO: docstring
+    if style == 'contest':
+        template = pkg_resources.resource_string(__name__, 'tests/data/single_contest_template.json')
+    elif style == 'election':
+        template = pkg_resources.resource_string(__name__, 'tests/data/election_template.json')
+    else:
+        raise click.BadArgumentUsage('No valid template style found')
+    if output is not None:
+        output.write(template)
+    else:
+        click.echo(template)
+
+
 def input_audit(contest: Contest, alpha: float = None, max_fraction_to_draw: float = None, audit_type: str = None) -> Audit:
-    """Create an audit from user-input."""
+    # Create an audit from user-input.
     click.echo('\nCreate a new Audit')
     click.echo('==================\n')
 
@@ -200,8 +303,7 @@ def input_audit(contest: Contest, alpha: float = None, max_fraction_to_draw: flo
 
 
 def input_contest() -> Contest:
-    """Creates a contest from user-input."""
-
+    # Creates a contest from user-input.
     click.echo('\nCreate a new Contest')
     click.echo('====================\n')
 
