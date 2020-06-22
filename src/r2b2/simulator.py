fd0b94109455f38e1b0acea001f52bcfd0059edf
sarahmorin <sarahmorin@gwu.edu>
2020-06-22 11:43:27 -0400

    basic simulation interface.
    added simulator.py which contains methods for simple mongo db operations
    and an abstract Simulation class definition. The idea is that
    simulations will be built off of Simulation with the same, very basic
    structure, and can be customized for specific cases. Ideally, when
    creating a new simulation the developer will not need to do/know much
    about the database operations and the functionality they need will be
    provided in simulator.py.

---
+++ src/r2b2/simulator.py
@@ -0,0 +1,163 @@
+"""R2B2 Simulation Module."""
+
+from abc import ABC
+from abc import abstractmethod
+
+from pymongo import MongoClient
+
+from r2b2.contest import Contest
+
+# FIXME: simply connects to local host
+client = MongoClient()
+db = client.r2b2
+
+
+def audit_lookup(audit_type: str, alpha: float, *args, **kwargs):
+    """Find/Create an audit in database.
+
+    Searches through database for an existing audit entry with the given parameters.
+    If none exists, an audit entry is created for the parameters.
+
+    Args:
+        audit_type (str): Name of audit, for example: 'minerva', 'brla', etc.
+        alpha (float): Risk-limit of audit.
+
+    Returns:
+        ObjectID of new or existing audit entry.
+    """
+    audits = db.audits
+    query = {'audit_type': audit_type, 'alpha': alpha}
+    # TODO: handle additional arguments
+
+    audit = audits.find_one(query)
+    if audit is not None:
+        return audit['_id']
+
+    return audits.insert_one(query).inserted_id
+
+
+def contest_lookup(contest, *args, **kwargs):
+    """Find/Create a contest in database.
+
+    Searches through database for an existing contest entry with the given parameters.
+    If none exists, a contest entry is created.
+
+    Args:
+        contest (r2b2.contest.Contest): Contest with attributes to be used in the database query.
+
+    Returns:
+        ObjectID of new of existing contest entry.
+    """
+    contests = db.contests
+    query = {
+        'contest_ballots': contest.contest_ballots,
+        'tally': contest.tally,
+        'num_winners': contest.num_winners,
+        'reported_winners': contest.reported_winners
+    }
+    # TODO: handle additional things
+    contest = contests.find_one(query)
+    if contest is not None:
+        return contest['_id']
+    return contests.insert_one(query).inserted_id
+
+
+def simulation_lookup(audit, reported, underlying, *args, **kwargs):
+    """Find/Create a simulation in database.
+
+    Searches through database for an existing simulation entry with the given parameters.
+    If none exists, a simulation entry is created.
+
+    Args:
+        audit: ObjectID of audit entry (from audits collection) used in the simulation.
+        reported: ObjectID of reported contest entry (from contests collection) used in the
+            simulation.
+        underlying: Description of the underlying contest used in the simulation. Could be an
+            ObjectID from the contests table, could simply be a string indicating a tie,
+            depends on the specific simulation.
+
+    Returns:
+        ObjectID of new or existing simulation entry.
+    """
+    simulations = db.simulations
+    query = {'reported': reported, 'underlying': underlying, 'audit': audit}
+    # TODO: Handle additional things
+    simulation = simulations.find_one(query)
+    if simulation is not None:
+        return simulation['_id']
+    return simulations.insert_one(query).inserted_id
+
+
+def trial_lookup(sim_id, *args, **kwargs):
+    """Find all trials for a given simulation ObjectID"""
+    return db.trials.find({'simulation': sim_id})
+
+
+def write_trial(entry):
+    """Write a trial document into the trials collection."""
+    db.trials.insert_one(entry)
+
+
+class Simulation(ABC):
+    """Abstract Base Class to define a simulation.
+
+    Attributes:
+        audit_type (str): Indicates what type of audit is simulated.
+        alpha (float): Risk-limit of simulation.
+        audit_id (str): ObjectID of audit entry from audits collection in MongoDB.
+        reported (Contest): Reported contest results that are audited during simulation.
+        reported_id (str): ObjectID of reported contest entry from contests collection in MongoDB.
+        underlying (str): Indicates the true underlying contest results ballots are drawn from
+            during the simulation. This might be an ObjectID similar to reported_id, it might be
+            a string simply indicating that the underlying distribution is a tie. This field is
+            specified by a specific simulation implementation.
+        sim_id (str): ObjectID of simulation from simulations collection in MongoDB defined by the
+            reported contest, underlying contest, and audit.
+    """
+
+    audit_type: str
+    alpha: float
+    audit_id: str
+    reported: Contest
+    reported_id: str
+    underlying: str
+    sim_id: str
+
+    def __init__(self, audit_type: str, alpha: float, reported: Contest, underlying):
+        self.audit_type = audit_type
+        self.alpha = alpha
+        self.audit_id = audit_lookup(audit_type, alpha)
+        self.reported = reported
+        self.reported_id = contest_lookup(reported)
+        self.underlying = underlying
+        self.sim_id = simulation_lookup(self.audit_id, self.reported_id, self.underlying)
+
+    def run(self, n: int):
+        """Execute n trials of the simulation.
+
+        Executes n simulation trials by generating a random seed, running a trial with the given
+        seed, and writing the trial entry to the trials collection.
+
+        Args:
+            n (int): Number of trials to execute and write to database.
+        """
+        for i in range(n):
+            curr_seed = self.get_seed()
+            trial_entry = {'simulation': self.sim_id, 'seed': curr_seed}
+            trial_entry.update(self.trial(curr_seed))
+            write_trial(trial_entry)
+
+    @abstractmethod
+    def trial(self, seed):
+        """Execute a single trial given a random seed."""
+        pass
+
+    @abstractmethod
+    def analyze(self, *args, **kwargs):
+        """Analyze the simulation trials."""
+        pass
+
+    @abstractmethod
+    def get_seed(self):
+        """Generate a random seed."""
+        pass
