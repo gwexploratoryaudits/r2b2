257b97b306e86cb589aed2d6fe9775140f84cca3
sarahmorin <sarahmorin@gwu.edu>
2020-08-04 12:41:32 -0400

    stricter click dependency requirement.
    needed stricter click requirement for installation. older versions
    created an error in the cli because of newer method call options.

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ setup(
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
     install_requires=[
         # eg: 'aspectlib==1.1.1', 'six>=1.7',
-        'Click',
+        'click>=7',
         'numpy',
         'scipy',
         'typing',
