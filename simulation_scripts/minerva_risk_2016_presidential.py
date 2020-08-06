15d4834bc85ef078deb2aff3067dabd224b9489d
sarahmorin <sarahmorin@gwu.edu>
2020-08-06 12:02:22 -0400

    initial minerva simulation.
    initial implementation of a one-round minerva simulation to compute risk
    (election is always tied) added. some changes will be made/fixed once
    the pairwise contest problem is resolved (in-progress in PR #29). A
    simple script to execute the 2016 presidential simulation trials added
    and the same script using filip's code has been renamed accordingly.

--- simulation_scripts/minerva_risk_2016_presidential.py
+++ simulation_scripts/minerva_risk_2016_presidential.py
@@ -1,34 +1,23 @@
 import json
 import logging
 
-from r2b2.simulation.filip_athena import FZMinervaOneRoundRisk as FZMOR
+from r2b2.simulation.minerva import MinervaOneRoundRisk as MORR
 from r2b2.tests.util import parse_election
 
-election_name = 'data/2016_presidential.json'
-election = parse_election(election_name)
+election = parse_election('data/2016_presidential.json')
 sample_size_file = 'data/2016_presidential_one_round_sample_sizes.json'
 
 
 def state_trial(state, alpha, sample_size):
-    sim = FZMOR(alpha,
-                election.contests[state],
-                sample_size,
-                election_name,
-                state,
-                sim_args={'description': 'One round Minerva (from fz athena repo) with given sample size (from PV MATLAB)'},
-                reported_args={
-                    'name': state,
-                    'description': '2016 Presidential'
-                })
+    sim = MORR(alpha, election.contests[state], sample_size, reported_name=state, sim_args={'description': 'One round Minerva with given sample size (from PV MATLAB)'}, reported_args={'name': state, 'description': '2016 Presidential'})
     sim.run(10000)
 
 
-if __name__ == '__main__':
+if __name__=='__main__':
     with open(sample_size_file, 'r') as fd:
         sample_sizes = json.load(fd)
         for contest in election.contests.keys():
             if contest == 'Michigan':
-                logging.warning('Michigan does not have sample sizes from MATLAB, so its simulation is not run.')
+                logging.warning('Michigan does not have sample sizes scaled from MATLAB, so its simulation is not run.')
                 continue
             sample_size = sample_sizes[contest]['Athena_pv_scaled']
-            state_trial(contest, 0.1, sample_size)
