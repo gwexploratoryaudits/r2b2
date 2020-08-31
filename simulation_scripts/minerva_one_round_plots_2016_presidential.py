639699188b25617483d88d45b300c0eabec5b0cf
sarahmorin <sarahmorin@gwu.edu>
2020-08-31 12:19:28 -0400

    added plots and analysis output.
    added ratio v margin plot. added summary analysis with and without DC
    since it is so odd.

--- simulation_scripts/minerva_one_round_plots_2016_presidential.py
+++ simulation_scripts/minerva_one_round_plots_2016_presidential.py
@@ -13,7 +13,10 @@ if __name__ == '__main__':
     db = DBInterface(user='reader', pwd='icanread')
     risks = []
     sprobs = []
+    ratios = []
     margins = []
+    dc_risk = 0.0
+    dc_sprob = 0.0
     
     for contest in election.contests:
         if contest == 'Michigan' or contest == 'New Hampshire':
@@ -26,9 +29,14 @@ if __name__ == '__main__':
 
         risks.append(tied_sim['analysis'])
         sprobs.append(sprob_sim['analysis'])
+        ratios.append(sprob_sim['analysis']/tied_sim['analysis'])
 
         winner_prop = election.contests[contest].tally[election.contests[contest].reported_winners[0]]/sum(election.contests[contest].tally.values())
         margins.append(winner_prop - (1.0 - winner_prop))
+
+        if contest == 'District of Columbia':
+            dc_risk = tied_sim['analysis']
+            dc_sprob = sprob_sim['analysis']
 
     # Plot risks vs. margins
     plt.plot(margins, risks, 'bo')
@@ -44,9 +52,24 @@ if __name__ == '__main__':
     plt.grid()
     plt.show()
 
+    # Plot ratio vs. margin
+    plt.plot(margins, ratios, 'bo')
+    plt.xlabel('Reported Margin')
+    plt.ylabel('Ratio Stop. Prob. to Risk (90% One-round Minerva, Alpha=10%)')
+    plt.grid()
+    plt.show()
+
     # Histogram of sprobs
     histogram(sprobs, 'Stopping Probabilities (90% One-round Minerva)', bins=20)
 
     # Histogram of risks
     histogram(risks, 'Experimental Risks (One-round Minerva, Alpha=10%)', bins=20)
+
+
+    # Analysis
+    avg_risk = 'Average Risk: {:%}\n'.format(sum(risks)/len(risks))
+    avg_sprob = 'Average Stopping Prob: {:%}\n'.format(sum(sprobs)/len(sprobs))
+    no_dc_risk = 'Average Risk (without DC): {:%}\n'.format((sum(risks)-dc_risk)/(len(risks)-1))
+    no_dc_sprob = 'Average Stopping Prob (without DC): {:%}\n'.format((sum(sprobs)-dc_sprob)/(len(sprobs)-1))
+    print(avg_risk + avg_sprob + no_dc_risk + no_dc_sprob)
 
