87fc2f02d92e10c6fedcf165d9ae4f4307849736
GrantMcClearn <grantmcc1@gwu.edu>
2020-09-11 17:55:53 -0400

    Implement interactive Athena - Imeplement interactive Athena for cli use - Add additional tests

--- src/r2b2/tests/test_athena.py
+++ src/r2b2/tests/test_athena.py
@@ -1,6 +1,10 @@
+import math
+
 import pytest
+from click.testing import CliRunner
 
 from r2b2.athena import Athena
+from r2b2.cli import cli
 from r2b2.contest import Contest
 from r2b2.contest import ContestType
 from r2b2.minerva import Minerva
@@ -29,6 +33,37 @@ def test_athena_minerva_paper():
     # From Athena paper
     assert athena.min_winner_ballots == [32]
     assert minerva.min_winner_ballots == [31]
+
+
+def test_interactive_athena():
+    runner = CliRunner()
+    user_in = 'athena\n0.1\n0.1\n100000\n2\nA\n75000\nB\n25000\n1\nA\nMAJORITY\ny\n1\ny\n50\nn\n31\nn\n100\nn\n70\n'
+    result = runner.invoke(cli, 'interactive', input=user_in)
+    output_file = open('src/r2b2/tests/data/cli_test_expected_out_interactive_athena.txt', 'r')
+    expected_out = output_file.read()
+    assert result.output == expected_out
+    output_file.close()
+
+
+def test_bulk_athena():
+    # Same as Minerva (that is, delta = infinity)
+
+    # Ballot-by-ballot Minerva should yield identical stopping rules to BRAVO.
+    contest = Contest(100000, {'A': 60000, 'B': 40000}, 1, ['A'], ContestType.MAJORITY)
+    athena = Athena(.1, 2**31 - 1, .01, contest)
+    athena.compute_all_min_winner_ballots()
+    # p0 not hardcoded as .5 for scalability with odd total contest ballots.
+    p0 = (athena.contest.contest_ballots // 2) / athena.contest.contest_ballots
+    log_winner_multiplier = math.log(athena.contest.winner_prop / p0)
+    log_loser_multiplier = math.log((1 - athena.contest.winner_prop) / p0)
+    log_rhs = math.log(1 / athena.alpha)
+
+    for i in range(len(athena.rounds)):
+        n = athena.rounds[i]
+        kmin = athena.min_winner_ballots[i]
+        # Assert this kmin satisfies ratio, but a kmin one less does not.
+        assert kmin * log_winner_multiplier + (n - kmin) * log_loser_multiplier > log_rhs
+        assert (kmin - 1) * log_winner_multiplier + (n - kmin + 1) * log_loser_multiplier <= log_rhs
 
 
 def test_exceptions():
