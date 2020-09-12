a1597ce6b5f186279b43c3831f756d9bb0597f29
sarahmorin <sarahmorin@gwu.edu>
2020-09-12 19:02:50 -0400

    one round athena simulation
    one round athena risk and stopping probability simulations based on
    athena implementation. scripts to run both sets of simulations for the
    2016 presidential data.

---
+++ simulation_scripts/athena_risk_2016_presidential.py
@@ -0,0 +1,29 @@
+import json
+import logging
+
+from r2b2.simulation.athena import AthenaOneRoundRisk as AORR
+from r2b2.tests.util import parse_election
+
+election = parse_election('data/2016_presidential.json')
+sample_size_file = 'data/2016_presidential_one_round_sample_sizes.json'
+
+
+def state_trial(state, alpha, sample_size):
+    sim = AORR(alpha, 1.0, election.contests[state], sample_size, sim_args={'description': 'One round Athena with given sample size (from PV MATLAB)'}, reported_args={'name': state, 'description': '2016 Presidential'})
+    sim.run(10000)
+    return sim.analyze()
+
+
+if __name__=='__main__':
+    with open(sample_size_file, 'r') as fd:
+        sample_sizes = json.load(fd)
+        for contest in election.contests.keys():
+            if contest == 'Michigan':
+                logging.warning('Michigan does not have sample sizes scaled from MATLAB, so its simulation is not run.')
+                continue
+            if contest == 'New Hampshire':
+                logging.warning('New Hampshire margin too small to compute min_sample_size efficiently, skipping.')
+                continue
+            sample_size = sample_sizes[contest]['Athena_pv_scaled']
+            computed_risk = state_trial(contest, 0.1, sample_size)
+            logging.info('{}: {}'.format(contest, computed_risk))
