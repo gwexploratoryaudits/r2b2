f91e7e902554bfd08d42f0343cdfa6f9aadfeeee
sarahmorin <sarahmorin@gwu.edu>
2020-10-25 15:42:54 -0400

    corrected formatting

--- simulation_scripts/minerva_one_round_plots_2016_presidential.py
+++ simulation_scripts/minerva_one_round_plots_2016_presidential.py
@@ -1,6 +1,5 @@
 """Generates plots, histograms for the 1-round Minerva risk, stopping probability sims."""
 
-import json
 import matplotlib.pyplot as plt
 
 from r2b2.simulator import DBInterface
@@ -17,21 +16,34 @@ if __name__ == '__main__':
     margins = []
     dc_risk = 0.0
     dc_sprob = 0.0
-    
+
     for contest in election.contests:
         if contest == 'Michigan' or contest == 'New Hampshire':
             continue
 
         audit_id = db.audit_lookup('minerva', 0.1)
         reported_id = db.contest_lookup(election.contests[contest], qapp={'description': '2016 Presidential'})
-        tied_sim  = db.db.simulations.find_one({'reported': reported_id, 'underlying': 'tie', 'audit': audit_id, 'description': 'One round Minerva with given sample size (from PV MATLAB)', 'invalid_ballots': True})
-        sprob_sim = db.db.simulations.find_one({'reported': reported_id, 'underlying': 'reported', 'audit': audit_id, 'description': 'Stopping Probability 90%: One round Minerva with given sample size (from PV MATLAB)', 'invalid_ballots': True})
+        tied_sim = db.db.simulations.find_one({
+            'reported': reported_id,
+            'underlying': 'tie',
+            'audit': audit_id,
+            'description': 'One round Minerva with given sample size (from PV MATLAB)',
+            'invalid_ballots': True
+        })
+        sprob_sim = db.db.simulations.find_one({
+            'reported': reported_id,
+            'underlying': 'reported',
+            'audit': audit_id,
+            'description': 'Stopping Probability 90%: One round Minerva with given sample size (from PV MATLAB)',
+            'invalid_ballots': True
+        })
 
         risks.append(tied_sim['analysis'])
         sprobs.append(sprob_sim['analysis'])
-        ratios.append(sprob_sim['analysis']/tied_sim['analysis'])
+        ratios.append(sprob_sim['analysis'] / tied_sim['analysis'])
 
-        winner_prop = election.contests[contest].tally[election.contests[contest].reported_winners[0]]/sum(election.contests[contest].tally.values())
+        winner_prop = election.contests[contest].tally[election.contests[contest].reported_winners[0]] / sum(
+            election.contests[contest].tally.values())
         margins.append(winner_prop - (1.0 - winner_prop))
 
         if contest == 'District of Columbia':
@@ -65,11 +77,9 @@ if __name__ == '__main__':
     # Histogram of risks
     histogram(risks, 'Experimental Risks (One-round Minerva, Alpha=10%)', bins=20)
 
-
     # Analysis
-    avg_risk = 'Average Risk: {:%}\n'.format(sum(risks)/len(risks))
-    avg_sprob = 'Average Stopping Prob: {:%}\n'.format(sum(sprobs)/len(sprobs))
-    no_dc_risk = 'Average Risk (without DC): {:%}\n'.format((sum(risks)-dc_risk)/(len(risks)-1))
-    no_dc_sprob = 'Average Stopping Prob (without DC): {:%}\n'.format((sum(sprobs)-dc_sprob)/(len(sprobs)-1))
+    avg_risk = 'Average Risk: {:%}\n'.format(sum(risks) / len(risks))
+    avg_sprob = 'Average Stopping Prob: {:%}\n'.format(sum(sprobs) / len(sprobs))
+    no_dc_risk = 'Average Risk (without DC): {:%}\n'.format((sum(risks) - dc_risk) / (len(risks) - 1))
+    no_dc_sprob = 'Average Stopping Prob (without DC): {:%}\n'.format((sum(sprobs) - dc_sprob) / (len(sprobs) - 1))
     print(avg_risk + avg_sprob + no_dc_risk + no_dc_sprob)
-
