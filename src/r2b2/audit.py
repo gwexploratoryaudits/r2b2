Refactor round estimate implementation. - Change round estimate implementation. - Fix bugs dealing with numerical precision. - Add Gaussian estimate for first round sizes.

src/r2b2/audit.py
@@ -6,7 +6,7 @@ from typing import List
 
 import click
 import numpy as np
-from scipy.signal import fftconvolve
+from scipy.signal import convolve
 from scipy.stats import binom
 from scipy.stats import hypergeom
 
@@ -136,7 +136,7 @@ class Audit(ABC):
             if len(self.rounds) == 1:
                 self.distribution_null = distribution_round_draw
             else:
-                self.distribution_null = fftconvolve(self.distribution_null, distribution_round_draw)
+                self.distribution_null = convolve(self.distribution_null, distribution_round_draw, method='direct')
         else:
             half_contest_ballots = math.floor(self.contest.contest_ballots / 2)
             if len(self.rounds) == 1:
@@ -174,7 +174,7 @@ class Audit(ABC):
             if len(self.rounds) == 1:
                 self.distribution_reported_tally = distribution_round_draw
             else:
-                self.distribution_reported_tally = fftconvolve(self.distribution_reported_tally, distribution_round_draw)
+                self.distribution_reported_tally = convolve(self.distribution_reported_tally, distribution_round_draw, method='direct')
         else:
             reported_winner_ballots = int(self.contest.winner_prop * self.contest.contest_ballots)
             if len(self.rounds) == 1:

src/r2b2/minerva.py
@@ -3,8 +3,9 @@ import math
 from typing import List
 
 import click
-from scipy.signal import fftconvolve
+from scipy.signal import convolve
 from scipy.stats import binom
+from scipy.stats import norm
 
 from r2b2.audit import Audit
 from r2b2.contest import Contest
@@ -30,6 +31,7 @@ class Minerva(Audit):
 
     def __init__(self, alpha: float, max_fraction_to_draw: float, contest: Contest):
         """Initialize a Minerva audit."""
+
         super().__init__(alpha, 0.0, max_fraction_to_draw, True, contest)
         self.min_sample_size = self.get_min_sample_size()
 
@@ -44,20 +46,7 @@ class Minerva(Audit):
             int: The minimum sample size of the audit, adherent to the min_sprob.
         """
 
-        # p0 is not .5 for contests with odd total ballots.
-        p0 = (self.contest.contest_ballots // 2) / self.contest.contest_ballots
-        p1 = self.contest.winner_prop
-        max_sample_size = math.ceil(self.contest.contest_ballots * self.max_fraction_to_draw)
-
-        # There may not be a point n' such that all n >= n' are acceptable round sizes and all
-        # n < n' are unacceptable round sizes.
-        for n in range(1, max_sample_size):
-            num_dist = binom.pmf(range(n+1), n, p1)
-            denom_dist = binom.pmf(range(n+1), n, p0)
-            if self.satisfactory_sample_size(n // 2, n+1, min_sprob, num_dist, denom_dist):
-                return n
-
-        return max_sample_size
+        return self.next_sample_size(sprob=min_sprob)
 
     def satisfactory_sample_size(self, left, right, sprob, num_dist, denom_dist):
         """Helper method that returns True if the round size satisfies the stopping probability."""
@@ -92,97 +81,58 @@ class Minerva(Audit):
         y = math.log(self.contest.winner_prop / (1 - self.contest.winner_prop))
         return math.ceil((-n * math.log(x) - math.log(self.alpha)) / y)
 
-    def sample_size_kmin(self, left, right, num_dist, denom_dist):
+    def sample_size_kmin(self, left, right, num_dist, denom_dist, sum_num_right, sum_denom_right, orig_right):
         """Finds a kmin with a binary search given the twin distributions."""
         if left > right:
             return 0
 
         mid = (left + right) // 2
 
-        sum_num = sum(num_dist[mid:])
-        sum_denom = sum(denom_dist[mid:])
+        sum_num = sum(num_dist[mid:orig_right]) + sum_num_right
+        sum_denom = sum(denom_dist[mid:orig_right]) + sum_denom_right
         satisfies_risk = self.alpha * sum_num > sum_denom
 
         sum_num_prev = sum_num + num_dist[mid-1]
         sum_denom_prev = sum_denom + denom_dist[mid-1]
-        satisfies_risk_prev = self.alpha * sum_num_prev > sum_denom_prev
+        satisfies_risk_prev = self.alpha * (sum_num_prev + 0) > (sum_denom_prev + 0)
 
         if satisfies_risk and not satisfies_risk_prev:
             return mid
         elif satisfies_risk and satisfies_risk_prev:
-            return self.sample_size_kmin(left, mid - 1, num_dist, denom_dist)
+            return self.sample_size_kmin(left, mid - 1, num_dist, denom_dist, sum_num_right, sum_denom_right, orig_right)
         elif not satisfies_risk and not satisfies_risk_prev:
-            return self.sample_size_kmin(mid + 1, right, num_dist, denom_dist)
+            return self.sample_size_kmin(mid + 1, right, num_dist, denom_dist, sum_num_right, sum_denom_right, orig_right)
         else:
             raise Exception("sample_size_sprob: k not monotonic.")
 
-    def sample_size_kmin_linear(self, num_dist, denom_dist):
-        """Finds a kmin with a linear search, from a smart starting point."""
-
-        start = int((.5 * len(num_dist) + self.contest.winner_prop * len(num_dist)) / 2)
-
-        sum_num = sum(num_dist[start:])
-        sum_denom = sum(denom_dist[start:])
-
-        start_pass = self.alpha * sum_num > sum_denom
-        start_prev_pass = self.alpha * (sum_num + num_dist[start - 1]) > (sum_denom + denom_dist[start - 1])
-        if start_pass and not start_prev_pass:
-            return start
-
-        if start_pass:
-            for k in range(start - 1, len(num_dist) // 2, -1):
-                sum_num += num_dist[k]
-                sum_denom += denom_dist[k]
-                satisfies_risk = self.alpha * sum_num > sum_denom
-                satisfies_risk_prev = self.alpha * (sum_num + num_dist[k - 1]) > (sum_denom + denom_dist[k - 1])
-                if satisfies_risk and not satisfies_risk_prev:
-                    return k
-        else:
-            for k in range(start, len(num_dist) + 1):
-                sum_num -= num_dist[k]
-                sum_denom -= denom_dist[k]
-                satisfies_risk = self.alpha * sum_num > sum_denom
-                satisfies_risk_prev = self.alpha * (sum_num + num_dist[k]) > (sum_denom + denom_dist[k])
-                if satisfies_risk and not satisfies_risk_prev:
-                    return k + 1
-
-        return 0
-
-    def binary_search_estimate(self, left, right, sprob):
-        """Method to use binary search approximation to find a round size estimate."""
-
-        # No acceptable round size.
-        if right - left <= 1:
-            return 0
-
-        mid = (left + right) // 2
-
+    def find_sprob(self, n):
+        """Helper method to find the stopping probability of a given prospective round size."""
         p0 = (self.contest.contest_ballots // 2) / self.contest.contest_ballots
         p1 = self.contest.winner_prop
 
         # The number of ballots that will be drawn this round.
         if len(self.rounds) > 0:
-            round_draw = mid - self.rounds[-1]
+            round_draw = n - self.rounds[-1]
         else:
-            round_draw = mid
+            round_draw = n
 
-        # We generate the distributions for this would-be round size.
         num_dist_round_draw = binom.pmf(range(0, round_draw + 1), round_draw, p1)
         denom_dist_round_draw = binom.pmf(range(0, round_draw + 1), round_draw, p0)
         if len(self.rounds) > 0:
-            num_dist = fftconvolve(self.distribution_reported_tally, num_dist_round_draw)
-            denom_dist = fftconvolve(self.distribution_null, denom_dist_round_draw)
+            num_dist = convolve(self.distribution_reported_tally, num_dist_round_draw, method='fft')
+            denom_dist = convolve(self.distribution_null, denom_dist_round_draw, method='fft')
         else:
             num_dist = num_dist_round_draw
             denom_dist = denom_dist_round_draw
 
         # We find the kmin for this would-be round size.
-        kmin = self.sample_size_kmin(len(num_dist) // 2, min(self.kmin_search_upper_bound(mid), len(num_dist)),
-                                     num_dist, denom_dist)
+        right = min(self.kmin_search_upper_bound(n), len(num_dist))
+        kmin = self.sample_size_kmin(len(num_dist) // 2, right, num_dist, denom_dist, sum(num_dist[right:]),
+                                     sum(denom_dist[right:]), right)
 
         # If there isn't a kmin, clearly we need a larger round size.
         if kmin == 0:
-            return self.binary_search_estimate(mid + 1, right, sprob)
+            return 0, 0.0
 
         # For the audit to stop, we need to get kmin winner ballots minus the winner ballots we already have.
         if len(self.rounds) > 0:
@@ -194,67 +144,91 @@ class Minerva(Audit):
         # as are needed? That is the stopping probability.
         sprob_round = sum(num_dist_round_draw[needed:])
 
-        # We repeat the above procedure for the a round size one less than above.
-        if len(self.rounds) > 0:
-            round_draw_prev = (mid - 1) - self.rounds[-1]
-        else:
-            round_draw_prev = mid - 1
+        return kmin, sprob_round
 
-        num_dist_round_draw_prev = binom.pmf(range(0, round_draw_prev + 1), round_draw_prev, p1)
-        denom_dist_round_draw_prev = binom.pmf(range(0, round_draw_prev + 1), round_draw_prev, p0)
+    def binary_search_estimate(self, left, right, sprob):
+        """Method to use binary search approximation to find a round size estimate."""
 
-        if len(self.rounds) > 0:
-            num_dist_prev = fftconvolve(self.distribution_reported_tally, num_dist_round_draw_prev)
-            denom_dist_prev = fftconvolve(self.distribution_null, denom_dist_round_draw_prev)
-        else:
-            num_dist_prev = num_dist_round_draw_prev
-            denom_dist_prev = denom_dist_round_draw_prev
+        # An additional check to ensure proper input.
+        if left > right:
+            return 0, 0, 0.0
 
-        kmin_prev = self.sample_size_kmin(len(num_dist_prev) // 2,
-                                          min(self.kmin_search_upper_bound(mid - 1),
-                                          len(num_dist)), num_dist_prev, denom_dist_prev)
-        if kmin_prev == 0:
-            sprob_prev = 0
-        else:
-            if len(self.rounds) > 0:
-                needed_prev = kmin_prev - self.sample_winner_ballots[-1]
-            else:
-                needed_prev = kmin_prev
-            sprob_prev = sum(num_dist_round_draw_prev[needed_prev:])
+        mid = (left + right) // 2
+
+        sprob_kmin_pair = self.find_sprob(mid)
 
         # This round size is returned if it has satisfactory stopping probability and a round size one less
         # does not, or if it has satisfactory stopping probability and exceeds the desired stopping probability
         # only nominally.
-        if (sprob_round >= sprob and sprob_round - sprob <= .0001) or (sprob_round >= sprob and sprob_prev < sprob):
-            return mid
-        elif sprob_round < sprob:
-            return self.binary_search_estimate(mid, right, sprob)
-        else:
-            return self.binary_search_estimate(left, mid, sprob)
+        if right - left <= 1:
+            if (sprob_kmin_pair[1] >= sprob):
+                assert sprob_kmin_pair[0] > 0
+                return mid, sprob_kmin_pair[0], sprob_kmin_pair[1]
+            else:
+                right_sprob_kmin_pair = self.find_sprob(mid + 1)
+                if right_sprob_kmin_pair[1] >= sprob:
+                    return mid + 1, self.find_sprob(mid + 1)[0], self.find_sprob(mid + 1)[1]
 
-    def next_sample_size(self, sprob=.9, *args, **kwargs):
+            return 0, 0, 0.0
+
+        if sprob_kmin_pair[1] >= sprob:
+            return self.binary_search_estimate(left, mid, sprob)
+        else:
+            return self.binary_search_estimate(mid, right, sprob)
+
+    def next_sample_size_gaussian(self, sprob=.9):
+        """This is a rougher but quicker round size estimate for very narrow margins."""
+        z_a = norm.isf(sprob)
+        z_b = norm.isf(self.alpha * sprob)
+        p = self.contest.winner_prop
+        return math.ceil(((z_a * math.sqrt(p * (1 - p)) - .5 * z_b) / (p - .5)) ** 2)
+
+    def next_sample_size(self, sprob=.9, verbose=False, *args, **kwargs):
         """
         Attempt to find a next sample size estimate no greater than 10000.
         Failing that, try to find an estimate no greater than 20000, and so on.
+
+        Args:
+            verbose (bool): If true, the kmin and stopping probability of the next sample size will be returned
+            in addition to the next sample size itself.
         """
 
+        # If the audit has already terminated, there is no next_sample_size.
+        if len(self.min_winner_ballots) > 0 and self.sample_winner_ballots[-1] >= self.min_winner_ballots[-1]:
+            if verbose:
+                return self.rounds[-1], 0, 1
+            return self.rounds[-1]
+
         # NOTE: Numerical issues arise when sample results disagree to an extreme extent with the reported margin.
-        upper_bound = 10000
+        start = 10000
+        if len(self.rounds) > 0:
+            init_upper_bound = self.get_upper_bound(self.rounds[-1] + 1, start)
+        else:
+            init_upper_bound = start
+        upper_bound = init_upper_bound
         while upper_bound < 10 ** 7:
             if len(self.rounds) > 0:
-                if upper_bound == 10000:
+                # Ensure upper bound is sufficiently large.
+                if upper_bound == init_upper_bound:
                     estimate = self.binary_search_estimate(self.rounds[-1] + 1, upper_bound, sprob)
                 else:
                     estimate = self.binary_search_estimate(upper_bound // 2, upper_bound, sprob)
             else:
-                if upper_bound == 10000:
-                    estimate = self.binary_search_estimate(self.min_sample_size, upper_bound, sprob)
+                if upper_bound == init_upper_bound:
+                    estimate = self.binary_search_estimate(1, upper_bound, sprob)
                 else:
                     estimate = self.binary_search_estimate(upper_bound // 2, upper_bound, sprob)
-            if estimate > 0:
-                return estimate
+            if estimate[0] > 0:
+                if verbose:
+                    return estimate
+                return estimate[0]
             upper_bound *= 2
         return 0
+
+    def get_upper_bound(self, n, start):
+        while start <= n:
+            start *= 2
+        return start
 
     def stopping_condition(self, votes_for_winner: int, verbose: bool = False) -> bool:
         """Check, without finding the kmin, whether the audit is complete."""
@@ -264,9 +238,10 @@ class Minerva(Audit):
         tail_null = sum(self.distribution_null[votes_for_winner:])
         tail_reported = sum(self.distribution_reported_tally[votes_for_winner:])
 
-        self.pvalue_schedule.append(tail_null / tail_reported)
+        pvalue = self.compute_risk(votes_for_winner)
+        self.pvalue_schedule.append(pvalue)
         if verbose:
-            click.echo('\np-value: {}'.format(tail_null / tail_reported))
+            click.echo('\np-value: {}'.format(pvalue))
             click.echo('\nRisk Level: {}'.format(self.get_risk_level()))
 
         return self.alpha * tail_reported > tail_null
@@ -285,7 +260,6 @@ class Minerva(Audit):
         Args:
             rounds (List[int]): A (partial) round schedule of the audit.
         """
-
         if len(rounds) < 1:
             raise ValueError('Round schedule must contain at least 1 round.')
 
@@ -382,6 +356,9 @@ class Minerva(Audit):
 
         tail_null = sum(self.distribution_null[votes_for_winner:])
         tail_reported = sum(self.distribution_reported_tally[votes_for_winner:])
+
+        if tail_reported == 0:
+            return 0
         return tail_null / tail_reported
 
     def get_risk_level(self):