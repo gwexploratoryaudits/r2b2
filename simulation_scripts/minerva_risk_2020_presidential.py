3829d7c73c9c2426926c7618d892966de00f3090
obroadrick <obroadrick@gwu.edu>
2021-02-15 15:48:19 -0500

    Add 2020 presidential results

---
+++ simulation_scripts/minerva_risk_2020_presidential.py
@@ -0,0 +1,39 @@
+import json
+import logging
+
+from r2b2.simulation.minerva import MinervaMultiRoundRisk as MMRR
+from r2b2.tests.util import parse_election
+
+
+election = parse_election('data/2020_presidential.json')
+sample_size_file = 'data/2020_presidential_one_round_sample_sizes.json'
+
+
+def state_trial(state, alpha, sample_size):
+    sim = MORR(alpha,
+               election.contests[state],
+               sample_size,
+               sample_multi = 1.5,
+               max_rounds = 10,
+               sim_args={'description': 'One round Minerva with given sample size (from PV MATLAB)'},
+               reported_args={
+                   'name': state,
+                   'description': '2016 Presidential'
+               })
+    sim.run(10000)
+    return sim.analyze()
+
+
+if __name__ == '__main__':
+    with open(sample_size_file, 'r') as fd:
+        sample_sizes = json.load(fd)
+        for contest in election.contests.keys():
+            if contest == 'Michigan':
+                logging.warning('Michigan does not have sample sizes scaled from MATLAB, so its simulation is not run.')
+                continue
+            if contest == 'New Hampshire':
+                logging.warning('New Hampshire margin too small to compute min_sample_size efficiently, skipping.')
+                continue
+            sample_size = sample_sizes[contest]['Athena_pv_scaled']
+            computed_risk = state_trial(contest, 0.1, sample_size)
+            logging.info('{}: {}'.format(contest, computed_risk))
